31ab3eb7bcede8c5d29f10bee18c202141885763
Youssoupha Diallo <youssoupha.d72@gmail.com>
2023-11-10 16:06:11 -0800

    removed block rigid body

--- Assets/Scripts/BlockingObject.cs
+++ Assets/Scripts/BlockingObject.cs
@@ -4,10 +4,12 @@ public class BlockingObject : MonoBehaviour
 {
     void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("BOX Collision detected");
+        // Debug.Log("BOX Collision detected");
         // Check if the collision was with the player object
         if (collision.gameObject.CompareTag("Player"))
         {
+            Debug.Log("PLAYER BOX Collision detected");
+            
             // Trigger the player collision handler script on the player object
             PlayerCollisionHandler playerCollisionHandler = collision.gameObject.GetComponent<PlayerCollisionHandler>();
             playerCollisionHandler.OnCollisionEnter(collision);
